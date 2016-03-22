bb3bd88ab38b16e95c992721abaf2738f61f4cd0
Ian Lin <andylin514@gmail.com>
2016-03-22 12:11:20 +0800

    finish

--- src/todo_list.js
+++ src/todo_list.js
@@ -2,9 +2,36 @@ class TodoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: ['123','456']
+      items: []
     };
   }
+
+  createNewItem(event) {
+    if (event.key === 'Enter') {
+      if (event.target.value !== '') {
+        // this.setState(this.state.items.push(event.target.value))
+        this.state.items.push({name: event.target.value, checked: false})
+        this.setState({items: this.state.items})
+        event.target.value = ''
+      }
+    }
+  }
+
+  deleteItem(idx) {
+    this.state.items.splice(idx, 1)
+    this.setState({items: this.state.items})
+  }
+
+  checkItem(idx) {
+    this.state.items[idx]['checked'] = !this.state.items[idx]['checked']
+    this.setState({items: this.state.items})
+  }
+
+  clearComplete() {
+    this.state.items = this.state.items.filter((item) => !item.checked)
+    this.setState({items: this.state.items})
+  }
+
   render() {
     const count = this.state.items.length;
     return (
@@ -12,18 +39,18 @@ class TodoApp extends React.Component {
         <section className="todoapp">
           <header className="header">
             <h1>todos</h1>
-            <input className="new-todo" placeholder="What needs to be done?" autofocus />
+            <input className="new-todo" placeholder="What needs to be done?" onKeyPress={this.createNewItem.bind(this)} autoFocus />
           </header>
           <section className="main">
             <input className="toggle-all" type="checkbox" />
             <label htmlFor="toggle-all">Mark all as complete</label>
             <ul className="todo-list">
-              {this.state.items.map((itemName) => <TodoItem itemName={itemName} />)}
+              {this.state.items.map((item, idx) => <TodoItem key={idx} item={item} onDelete={this.deleteItem.bind(this, idx)} onCheck={this.checkItem.bind(this, idx)} />)}
             </ul>
           </section>
           <footer className="footer">
             <CountDisplay count={count} />
-            <button className="clear-completed">Clear completed</button>
+            <button className="clear-completed" onClick={this.clearComplete.bind(this)}>Clear completed</button>
           </footer>
         </section>
         <footer className="info">
@@ -34,14 +61,15 @@ class TodoApp extends React.Component {
   }
 }
 
+
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={this.props.item.checked? 'completed': ''}>
         <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>{this.props.itemName}</label>
-          <button className="destroy"></button>
+          <input className="toggle" type="checkbox" checked={this.props.item.checked} onChange={this.props.onCheck} />
+          <label>{this.props.item.name}</label>
+          <button className="destroy" onClick={this.props.onDelete}></button>
         </div>
       </li>
     );
@@ -49,12 +77,13 @@ class TodoItem extends React.Component {
 }
 
 TodoItem.propTypes = {
-  itemName: React.PropTypes.string
+  item: React.PropTypes.object,
 };
 
 TodoItem.defaultProps = {
-  itemName: ''
+  item: {name: '', checked: false}
 };
+
 
 class CountDisplay extends React.Component {
   render() {
@@ -82,5 +111,6 @@ CountDisplay.propTypes = {
 CountDisplay.defaultProps = {
   count: 0
 };
+
 
 ReactDOM.render(<TodoApp />, document.getElementById('root'));
